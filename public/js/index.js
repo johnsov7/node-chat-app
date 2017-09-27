4e6edaf8f8208b8a3d4f3ca0cf0b300b830c8080
vincent <myworkplace7@gmail.com>
2017-09-27 13:37:20 -0500

    Added css to app

--- public/js/index.js
+++ public/js/index.js
@@ -32,11 +32,14 @@ socket.on('newLocationMessage', function (message) {
 jQuery(`#message-form`).on('submit', function (e) {
   e.preventDefault();
 
+  var messageTextBox = jQuery('[name-message]');
+
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextBox.val()
 
   }, function () {
+    messageTextBox.val('')
 
   });
 });
